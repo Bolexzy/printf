d13e172693815f6705479edb3ff972d7a1427f64
Bolexzy <boluoyewumi79@gmail.com>
2022-10-16 02:26:10 +0100

    Fix print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-/**/
+/**
+ * _printfBin - Print an unsigned int in base 2.
+ *
+ * @args: Argument list(unsigned int)
+ * Return: Length og binary numbers printed.
+ */
 int _printfBin(va_list args)
 {
 	char *bin_buff;
@@ -9,5 +14,5 @@ int _printfBin(va_list args)
 	bin_buff = litoa(va_arg(args, int), 2);
 	bin_len = print((bin == NULL) ? "(NULL)" : bin);
 
-	return bin_len;
+	return (bin_len);
 }
