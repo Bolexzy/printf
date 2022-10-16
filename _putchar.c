dc243a4f508664eb5d4f2272b469de81fd979a5e
Bolexzy <boluoyewumi79@gmail.com>
2022-10-16 17:10:26 +0100

    Add buffer for write

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,28 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (buffer(c));
+}
+
+/**
+ * buffer - Store the character in a buffer.
+ * @c: Character
+ *
+ * Return: 1
+ */
+int buffer(char c)
+{
+	static char buffer[1024];
+	static char i;
+
+	if (c == -1 || i = 1024)
+	{
+		write(1, buffer, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buffer[i++] = c;
+
+	return (1);
 }
