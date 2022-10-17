3ed67eeffdbbe9efb289190d21e6a415cc3e91f9
Bolexzy <boluoyewumi79@gmail.com>
2022-10-17 17:35:42 +0100

    Fix print_hexstring.c

--- print_hexstring.c
+++ print_hexstring.c
@@ -11,7 +11,7 @@ int _printfStringHex(va_list args)
 	char *s;
 	char *s_buf;
 	int s_len;
-	unsigned int i;
+	unsigned int i, tcast;
 
 	s = va_arg(args, char *);
 
@@ -27,20 +27,21 @@ int _printfStringHex(va_list args)
 			_putchar('\\');
 			_putchar('x');
 			s_len += 2;
-			if ((int)s[i] < 16)
+			tcast = s[i];
+			if (tcast < 16)
 			{
 				_putchar ('0');
 				s_len++;
 			}
-			s_buf = litoa((unsigned int)s[i], 16);
+			s_buf = litoa(tcast, 16);
 			s_len += print((s_buf == NULL) ? "(NULL)" : s_buf);
 		}
 		else
 		{
 			_putchar(s[i]);
+			s_len++;
 		} i++;
 	}
-	s_len += i;
 
 	return (s_len);
 }
