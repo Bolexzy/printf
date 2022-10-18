1fa1e8ca1c79ed7240b499ba79cba7cc3d28c461
horlami228 <akintolaolamilekan51@gmail.com>
2022-10-18 13:51:11 +0100

    Update int_utils.c

--- int_utils.c
+++ int_utils.c
@@ -12,6 +12,28 @@ int _isdigit(int c)
 }
 
 /**
+ * _printf_address - prints address
+ *
+ * @arg: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
+int _printf_address(va_list arg, format_t *params)
+{
+	unsigned long int n = va_arg(arg, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	str = litoa(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
+
+/**
  * litoa - Converts integer to ascii
  *
  * @num: Input integer.
