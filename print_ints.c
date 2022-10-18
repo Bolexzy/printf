508db351a8ab184421b197eb174f0f8f395df24c
Bolexzy <boluoyewumi79@gmail.com>
2022-10-18 13:26:18 +0100

    Add unsigned int func

--- print_ints.c
+++ print_ints.c
@@ -21,6 +21,28 @@ int _printfInt(va_list arg, format_t *params)
 }
 
 /**
+ * _printf_unsigned - prints unsigned integer numbers
+ *
+ * @arg: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
+int _printf_unsigned(va_list arg, format_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(arg, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(arg, unsigned int);
+	else
+		l = (unsigned int)va_arg(arg, unsigned int);
+	params->unsign = 1;
+	return (print_number(litoa(l, 10, CONVERT_UNSIGNED, params), params));
+}
+
+/**
  * print_number - prints a number with options
  * @str: the base number as a string
  * @params: the parameter struct
